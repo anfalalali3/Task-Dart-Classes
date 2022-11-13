a033b8a8bafa90eee3bf2cc4ee5bfe82939efd8a
Anfal <ahassan2@mof.gov.kw>
2022-11-13 19:16:35 +0300

    finished the task

--- main.dart
+++ main.dart
@@ -1,10 +1,22 @@
+class Movie {
+  String name, genre;
+  double ratings;
+  Movie({required this.name, required this.genre, required this.ratings});
+  void printDetails() {
+    print("Movie:${this.name},genres:${this.genre},ratings:${this.ratings}");
+  }
+}
+
 void main() {
   final movie = {
     'name': 'The Amazing SpiderMan',
     'genre': 'Action',
-    'ratings': [1.5,3.2,6.4]
+    'ratings': [1.5, 3.2, 6.4]
   };
   print(movie['name']);
   print(movie['genre']);
   print(movie['ratings']);
+
+  Movie Cinderella = Movie(name: "Cinderella", genre: "Fantasy", ratings: 10);
+  Cinderella.printDetails();
 }
